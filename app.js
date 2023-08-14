37a4784cecca521393d5fd5bb66773ded03854c5
nayeong <nyk1118@naver.com>
2023-08-14 17:46:39 +0900

    chore: set up initial structure

---
+++ app.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const createError = require("http-errors");
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+mongoose.connect(process.env.MONGODB_URI);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors(process.env.CORS_ORIGIN));
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+module.exports = app;
