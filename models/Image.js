275825cc1ecf2a95347fa35df666267bb8a115c4
nayeong <nyk1118@naver.com>
2023-08-29 05:40:23 +0900

    feature: add colorGroup and set rgbColor required

--- models/Image.js
+++ models/Image.js
@@ -2,11 +2,12 @@ const mongoose = require("mongoose");
 
 const imageSchema = new mongoose.Schema({
   url: { type: String, required: true, unique: true },
-  dominantColor: {
-    r: Number,
-    g: Number,
-    b: Number,
+  rgbColor: {
+    r: { type: Number, required: true },
+    g: { type: Number, required: true },
+    b: { type: Number, required: true },
   },
+  colorGroup: { type: Number, required: true },
 });
 
 module.exports = mongoose.model("Image", imageSchema);
