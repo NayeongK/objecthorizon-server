ea366475ce7cf7d9924d29ced2d8f146aa2aba8c
nayeong <nyk1118@naver.com>
2023-08-29 05:50:21 +0900

    feature: add GET method for fetching images with closest RGB

--- controllers/images.controller.js
+++ controllers/images.controller.js
@@ -1,5 +1,7 @@
 const Image = require("../models/Image");
 const { fetchImageUrlsFromS3 } = require("../services/imageService");
+const { getTargetGroups } = require("../utils/targetGroupCalculator");
+const { calculateDistance } = require("../utils/distanceCalculator");
 
 exports.get = async function (req, res, next) {
   try {
@@ -9,8 +11,52 @@ exports.get = async function (req, res, next) {
     );
 
     if (initial) {
-      res.status(200).json(imageURLs);
+      res.status(200).json(imageURLs.slice(0, 1));
+      return;
     }
+
+    const { color: colorStr } = req.query;
+    const color = colorStr.split(",").map(Number);
+    const rGroup = Math.floor(color[0] / 43) + 1;
+    const gGroup = Math.floor(color[1] / 43) + 1;
+    const bGroup = Math.floor(color[2] / 43) + 1;
+    const colorGroup = rGroup * 100 + gGroup * 10 + bGroup + 1;
+    const colorGroupArray = colorGroup.toString().split("").map(Number);
+    const currentImageRGB = [color[0], color[1], color[2]];
+
+    const targetGroups = getTargetGroups(
+      colorGroupArray[0],
+      colorGroupArray[1],
+      colorGroupArray[2],
+    );
+
+    const targetImages = await Image.find({
+      colorGroup: { $in: targetGroups },
+    });
+
+    let minDistance = Infinity;
+    let closestImage = null;
+
+    for (let i = 0; i < targetImages.length; i++) {
+      const targetRGB = [
+        targetImages[i].rgbColor.r,
+        targetImages[i].rgbColor.g,
+        targetImages[i].rgbColor.b,
+      ];
+      const distance = calculateDistance(currentImageRGB, targetRGB);
+
+      if (distance === 0) continue;
+
+      if (distance < minDistance) {
+        minDistance = distance;
+        closestImage = targetImages[i];
+      }
+    }
+    res.json(
+      closestImage
+        ? { url: closestImage.url }
+        : { message: "No matching image found" },
+    );
   } catch (err) {
     next(err);
     console.error(err);
@@ -22,19 +68,25 @@ exports.post = async function (req, res, next) {
     const { initial } = req.query;
     if (initial) {
       const { url, color } = req.body;
+
+      const rGroup = Math.floor(color[0] / 43) + 1;
+      const gGroup = Math.floor(color[1] / 43) + 1;
+      const bGroup = Math.floor(color[2] / 43) + 1;
       const existingImage = await Image.findOne({ url });
       if (!existingImage) {
         await new Image({
           url,
-          dominantColor: {
+          rgbColor: {
             r: color[0],
             g: color[1],
             b: color[2],
           },
+          colorGroup: rGroup * 100 + gGroup * 10 + bGroup,
         }).save();
+
+        res.json({ success: true });
       }
     }
-    res.json({ success: true });
   } catch (err) {
     next(err);
     console.error(err);
