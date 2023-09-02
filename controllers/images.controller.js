5884f521497171bc8ea66720e37328bed406093e
nayeong <nyk1118@naver.com>
2023-09-02 17:25:18 +0900

    refactor: search logic with KD-Tree for consistent performance

--- controllers/images.controller.js
+++ controllers/images.controller.js
@@ -1,10 +1,16 @@
 const Image = require("../models/Image");
 const { fetchImageUrlsFromS3 } = require("../services/imageService");
-const { getTargetGroups } = require("../utils/targetGroupCalculator");
-const { calculateDistance } = require("../utils/distanceCalculator");
+const { buildKDTree, closestPoint } = require("../utils/kd-tree");
 
 exports.get = async function (req, res, next) {
   try {
+    const images = await Image.find({});
+    const points = images.map(img => [
+      img.rgbColor.r,
+      img.rgbColor.g,
+      img.rgbColor.b,
+    ]);
+    const tree = buildKDTree(points);
     const { initial } = req.query;
     const imageURLs = await fetchImageUrlsFromS3(
       process.env.AWS_S3_BUCKET_NAME,
@@ -17,41 +23,15 @@ exports.get = async function (req, res, next) {
 
     const { color: colorStr } = req.query;
     const color = colorStr.split(",").map(Number);
-    const rGroup = Math.floor(color[0] / 43) + 1;
-    const gGroup = Math.floor(color[1] / 43) + 1;
-    const bGroup = Math.floor(color[2] / 43) + 1;
-    const colorGroup = rGroup * 100 + gGroup * 10 + bGroup + 1;
-    const colorGroupArray = colorGroup.toString().split("").map(Number);
-    const currentImageRGB = [color[0], color[1], color[2]];
 
-    const targetGroups = getTargetGroups(
-      colorGroupArray[0],
-      colorGroupArray[1],
-      colorGroupArray[2],
+    const closestImageColor = closestPoint(tree, color);
+    const closestImage = images.find(
+      img =>
+        img.rgbColor.r === closestImageColor[0] &&
+        img.rgbColor.g === closestImageColor[1] &&
+        img.rgbColor.b === closestImageColor[2],
     );
 
-    const targetImages = await Image.find({
-      colorGroup: { $in: targetGroups },
-    });
-
-    let minDistance = Infinity;
-    let closestImage = null;
-
-    for (let i = 0; i < targetImages.length; i++) {
-      const targetRGB = [
-        targetImages[i].rgbColor.r,
-        targetImages[i].rgbColor.g,
-        targetImages[i].rgbColor.b,
-      ];
-      const distance = calculateDistance(currentImageRGB, targetRGB);
-
-      if (distance === 0) continue;
-
-      if (distance < minDistance) {
-        minDistance = distance;
-        closestImage = targetImages[i];
-      }
-    }
     res.json(
       closestImage
         ? { url: closestImage.url }
---
+++ utils/kd-tree.js
@@ -0,0 +1,59 @@
+function buildKDTree(points, depth = 0) {
+  if (!points.length) return null;
+
+  const k = points[0].length;
+  const axis = depth % k;
+
+  points.sort((a, b) => a[axis] - b[axis]);
+  const median = Math.floor(points.length / 2);
+
+  const node = {
+    point: points[median],
+    left: buildKDTree(points.slice(0, median), depth + 1),
+    right: buildKDTree(points.slice(median + 1), depth + 1),
+    axis: axis,
+  };
+
+  return node;
+}
+
+function distanceSquared(pointA, pointB) {
+  let distance = 0;
+  for (let i = 0; i < pointA.length; i++) {
+    distance += (pointA[i] - pointB[i]) ** 2;
+  }
+  return distance;
+}
+
+function closestPoint(node, point, best = null) {
+  if (node === null) return best;
+
+  const d = distanceSquared(node.point, point);
+  const dx = node.point[node.axis] - point[node.axis];
+  let bestPoint = best;
+
+  if (best === null || d < distanceSquared(best, point)) {
+    bestPoint = node.point;
+  }
+
+  if (dx > 0) {
+    bestPoint = closestPoint(node.left, point, bestPoint);
+  } else {
+    bestPoint = closestPoint(node.right, point, bestPoint);
+  }
+
+  if (dx ** 2 < distanceSquared(bestPoint, point)) {
+    if (dx > 0) {
+      bestPoint = closestPoint(node.right, point, bestPoint);
+    } else {
+      bestPoint = closestPoint(node.left, point, bestPoint);
+    }
+  }
+
+  return bestPoint;
+}
+
+module.exports = {
+  buildKDTree,
+  closestPoint,
+};
