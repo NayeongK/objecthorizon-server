a08ef1446f8e51e7a8b3a2c8e4e730f9aa4bff91
nayeong <nyk1118@naver.com>
2024-01-11 11:21:11 +0900

    chore: mirgate images route to TypeScript

--- routes/images.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const imagesController = require("../controllers/images.controller.js");
-
-router.get("/", imagesController.get);
-router.post("/", imagesController.post);
-
-module.exports = router;
